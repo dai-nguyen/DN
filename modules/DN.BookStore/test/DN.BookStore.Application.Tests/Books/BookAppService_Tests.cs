e53e6ea40378919c55ea1a71ce6a0a5813d2eb43
Dai Nguyen <dai.t.nguyen@hotmail.com>
2023-03-16 22:48:57 -0700

    up-to-date

---
+++ modules/DN.BookStore/test/DN.BookStore.Application.Tests/Books/BookAppService_Tests.cs
@@ -0,0 +1,26 @@
+﻿using Shouldly;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DN.BookStore.Books
+{
+    public class BookAppService_Tests : BookStoreApplicationTestBase
+    {
+        private readonly IBookAppService _bookAppService;
+
+        public BookAppService_Tests()
+        {
+            _bookAppService = GetRequiredService<IBookAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_List_Of_Books()
+        {
+            var result = await _bookAppService.GetListAsync(
+                new GetBookListDto());
+
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.Items.ShouldContain(_ => _.Name == "1984");
+        }
+    }
+}
