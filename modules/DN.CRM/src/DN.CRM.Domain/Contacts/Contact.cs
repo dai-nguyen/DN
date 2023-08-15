7dd7ce19035b0924fda7ae29e1c9470a6c1136f1
Dai Nguyen <dai.t.nguyen@hotmail.com>
2023-08-14 22:46:54 -0700

    up-to-date

---
+++ modules/DN.CRM/src/DN.CRM.Domain/Contacts/Contact.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace DN.CRM.Contacts
+{
+    public class Contact : FullAuditedAggregateRoot<Guid>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        internal Contact(
+            Guid id,
+            [NotNull] string firstName,
+            [NotNull] string lastName,
+            [NotNull] string email,
+            [CanBeNull] string phone)
+        {
+
+        }
+
+    }
+
+    
+}
